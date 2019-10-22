b313864b4b6d17166cea446dbe635fd8e8583d60
Tomasz Łuczak <tlu@team-tl.pl>
2019-10-23 01:53:26 +0200

    2.6 missing script

---
+++ src/mswindows.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+""" Windows specific module """
+
+import platform
+
+def windows():
+    """ check system Windows or normal OS """
+    if platform.system() == "Windows":
+        result = 1
+    else:
+        result = 0
+    return result
+
+# EOF
