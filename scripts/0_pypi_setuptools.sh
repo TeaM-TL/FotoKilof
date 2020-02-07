d35a8486a904d4cfd711784f8636da1bdf5e4f2b
Tomasz Łuczak <tlu@team-tl.pl>
2020-02-08 00:32:11 +0100

    3.2.2

--- scripts/0_pypi_setuptools.sh
+++ scripts/0_pypi_setuptools.sh
@@ -2,13 +2,18 @@
 
 CWD=`pwd`
 INITPY="src/__init__.py"
+MAINPY="src/fotokilof.py"
+MAIN__PY="src/__main__.py"
 
 echo "- PyPI ---"
 
 cd ..
 touch $INITPY
+mv $MAINPY $MAIN__PY
+
 python3 setup.py bdist_wheel
 
+mv $MAIN__PY $MAINPY
 rm $INITPY
 cd $CWD
 
--- scripts/1_pypi_run.sh
+++ scripts/1_pypi_run.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
- python3 dist/FotoKilof-3.2.2-py3-none-any.whl/src
+#!/usr/bin/env bash
+VERSION=3.2.2
+python3 ../dist/FotoKilof-$VERSION-py3-none-any.whl/src &
+exit
