f30a1d0da6a2495375969f81192ed1289b999eec
Tomasz Łuczak <tlu@team-tl.pl>
2020-02-14 15:14:39 +0100

    3.3.2 added license into source files

--- src/convert.py
+++ src/convert.py
@@ -1,4 +1,27 @@
 # -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2019-2020 Tomasz Łuczak, TeaM-TL
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 """ Converters
 - convert_preview_crop_gravity - convert corrdinates from crop3
 - convert_border - add border to picture
@@ -12,6 +35,7 @@
 - gravity - translate eg. NS to Northsouth as Tk expect
 - gravity_outside - translate gravitation for adding text outside
 """
+
 
 def convert_preview_crop_gravity(coordinates, x_max, y_max):
     """
