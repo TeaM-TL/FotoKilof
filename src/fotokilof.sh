0082467e076d2dae5e80a82c87f21b688c69e849
Tomasz Łuczak <tlu@team-tl.pl>
2020-12-26 18:27:01 +0100

    3.5.0 added screenshot

--- src/fotokilof.sh
+++ src/fotokilof.sh
@@ -4,5 +4,19 @@
 MAIN=fotokilof.py
 MYPATH="${0%/*}"
 cd $MYPATH
+
+# compile MO files, usable for testing tanslations
+NAME=fotokilof
+cd locale/
+for I in ??; do
+    cd $I/LC_MESSAGES
+    if [ -e $NAME.po ]; then
+        msgfmt $NAME.po -o $NAME.mo
+    fi
+    cd ../../
+done
+cd ..
+
+# run FotoKilof
 python3 $MAIN &
 
