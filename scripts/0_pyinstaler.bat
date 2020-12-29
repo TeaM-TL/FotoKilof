160ddec0f1028598e9c2c094b1410b83b6193196
Tomasz Łuczak <tlu@team-tl.pl>
2020-12-29 17:10:10 +0100

    windows

--- scripts/0_pyinstaler.bat
+++ scripts/0_pyinstaler.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 cd ..
 rmdir build /S /Q
-rmdir src/__pycache__
+rmdir src\__pycache__
 SET UPX=--upx-dir upx
 SET UPX=--noupx
 SET ONEFILE=--onefile
 SET ONEFILE=
-pyinstaller --clean --console %ONEFILE% %UPX% src\fotokilof.py
+pyinstaller --noconfirm --clean --console %ONEFILE% %UPX% src\fotokilof.py
 pause
 cd scripts
