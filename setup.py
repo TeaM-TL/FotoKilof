45d2341b48b2bd6f56b9d71730fcd6a38ec094af
TLu <tlu@team-tl.pl>
2021-02-26 11:01:31 +0100

    3.5.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FotoKilof",
-    version="3.5.6b",
+    version="3.5.7",
     author="Tomasz Łuczak",
     author_email="tlu@team-tl.pl",
     description="Nice gui for ImageMagick",
