6449004d7ab83475e3b1d561653a16d3afa4b4e8
TLu <tlu@team-tl.pl>
2025-01-07 21:35:29 +0100

    5.1.6

--- scripts/0_pypi_setuptools.sh
+++ scripts/0_pypi_setuptools.sh
@@ -4,10 +4,19 @@ rm -rf ../build/*
 rm -rf ../fotokilof/__pycache__
 CWD=`pwd`
 
-echo "- PyPI ---"
+
 
 cd ..
+echo "- Locale ---"
+LC_LAST=LC_MESSAGES/fotokilof
+for L in fotokilof/locale/??; do
+    if [ -e $L/$LC_LAST.po ]; then
+        msgfmt $L/$LC_LAST.po -o $L/$LC_LAST.mo
+        ls $L/$LC_LAST*
+    fi
+done
 
+echo "- PyPI ---"
 python -m build --wheel
 cd $CWD
 
