c422f161d5ed3961c13bc47d723375c311a722fa
TLu <tlu@team-tl.pl>
2025-09-08 02:48:24 +0200

    5.1.10

--- fotokilof/gui_bindings.py
+++ fotokilof/gui_bindings.py
@@ -30,24 +30,25 @@ THE SOFTWARE.
 nice GUI for ImageMagick command common used (by me)
 """
 
+
 def setup_root_bindings(root, actions):
     """
     actions -- dictionary { 'action': function }, function is a callback, called by bind
     """
-    if 'help_info' in actions:
-        root.bind("<F1>", actions['help_info'])
-    if 'change_theme' in actions:
-        root.bind("<F2>", actions['change_theme'])
-    if 'open_file_prev_key' in actions:
-        root.bind("<Prior>", actions['open_file_prev_key'])
-    if 'open_file_next_key' in actions:
-        root.bind("<Next>", actions['open_file_next_key'])
-    if 'open_file_first_key' in actions:
-        root.bind("<Home>", actions['open_file_first_key'])
-    if 'open_file_last_key' in actions:
-        root.bind("<End>", actions['open_file_last_key'])
-    if 'close_program' in actions:
-        root.protocol("WM_DELETE_WINDOW", actions['close_program'])
+    if "help_info" in actions:
+        root.bind("<F1>", actions["help_info"])
+    if "change_theme" in actions:
+        root.bind("<F2>", actions["change_theme"])
+    if "open_file_prev_key" in actions:
+        root.bind("<Prior>", actions["open_file_prev_key"])
+    if "open_file_next_key" in actions:
+        root.bind("<Next>", actions["open_file_next_key"])
+    if "open_file_first_key" in actions:
+        root.bind("<Home>", actions["open_file_first_key"])
+    if "open_file_last_key" in actions:
+        root.bind("<End>", actions["open_file_last_key"])
+    if "close_program" in actions:
+        root.protocol("WM_DELETE_WINDOW", actions["close_program"])
 
 
 def setup_widget_bindings(widgets, actions, os_system):
@@ -57,22 +58,31 @@ def setup_widget_bindings(widgets, actions, os_system):
     widgets -- dictionary { name : widget }
     actions -- dictionary { name : function }
     """
-    # Crop na podglądzie oryginału 
-    if 'mouse_crop_nw' in actions and 'c_preview_orig_pi' in widgets:
-        widgets['c_preview_orig_pi'].bind("<Button-1>", actions['mouse_crop_nw'])
-    if 'mouse_crop_se' in actions and 'c_preview_orig_pi' in widgets:
+    # Crop na podglądzie oryginału
+    if "mouse_crop_nw" in actions and "c_preview_orig_pi" in widgets:
+        widgets["c_preview_orig_pi"].bind("<Button-1>", actions["mouse_crop_nw"])
+    if "mouse_crop_se" in actions and "c_preview_orig_pi" in widgets:
         btn_code = "<Button-2>" if os_system == "MACOS" else "<Button-3>"
-        widgets['c_preview_orig_pi'].bind(btn_code, actions['mouse_crop_se'])
+        widgets["c_preview_orig_pi"].bind(btn_code, actions["mouse_crop_se"])
     # Podgląd nowego obrazu
-    if 'preview_new_button' in actions and 'c_preview_new_pi' in widgets:
-        widgets['c_preview_new_pi'].bind("<Button-1>", actions['preview_new_button'])
+    if "preview_new_button" in actions and "c_preview_new_pi" in widgets:
+        widgets["c_preview_new_pi"].bind("<Button-1>", actions["preview_new_button"])
 
     # Comboboxy
-    if 'preview_orig_refresh' in actions and 'co_preview_selector_orig' in widgets:
-        widgets['co_preview_selector_orig'].bind("<<ComboboxSelected>>", actions['preview_orig_refresh'])
-    if 'preview_new_refresh' in actions and 'co_preview_selector_new' in widgets:
-        widgets['co_preview_selector_new'].bind("<<ComboboxSelected>>", actions['preview_new_refresh'])
-    if 'preview_compose_refresh' in actions and 'co_compose_preview_selector' in widgets:
-        widgets['co_compose_preview_selector'].bind("<<ComboboxSelected>>", actions['preview_compose_refresh'])
-    if 'font_selected' in actions and 'co_text_font' in widgets:
-        widgets['co_text_font'].bind("<<ComboboxSelected>>", actions['font_selected'])
+    if "preview_orig_refresh" in actions and "co_preview_selector_orig" in widgets:
+        widgets["co_preview_selector_orig"].bind(
+            "<<ComboboxSelected>>", actions["preview_orig_refresh"]
+        )
+    if "preview_new_refresh" in actions and "co_preview_selector_new" in widgets:
+        widgets["co_preview_selector_new"].bind(
+            "<<ComboboxSelected>>", actions["preview_new_refresh"]
+        )
+    if (
+        "preview_compose_refresh" in actions
+        and "co_compose_preview_selector" in widgets
+    ):
+        widgets["co_compose_preview_selector"].bind(
+            "<<ComboboxSelected>>", actions["preview_compose_refresh"]
+        )
+    if "font_selected" in actions and "co_text_font" in widgets:
+        widgets["co_text_font"].bind("<<ComboboxSelected>>", actions["font_selected"])
