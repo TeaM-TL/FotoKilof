0fe0767809775aaf75a5d1aabb7d7016e4d6aa80
TLu <tlu@team-tl.pl>
2025-11-30 21:04:09 +0100

    flatpak

--- flatpak/1_prepare_json_deps.sh
+++ flatpak/1_prepare_json_deps.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-python3 -m pip install --user requirements-parser
-python3 -m pip install --upgrade certifi
+sudo apt-get install python3-requirement-parser python3-certifi flatpak-builder
+
+#python3 -m pip install --user requirements-parser
+#python3 -m pip install --upgrade certifi
 export SSL_CERT_FILE=$(python3 -m certifi)
 
 curl -O https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/pip/flatpak-pip-generator.py
 chmod +x flatpak-pip-generator.py
 
-flatpak-builder --force-clean build-dir manifest.yml
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak update
+sudo flatpak install -y flathub org.freedesktop.Sdk//25.08
+sudo flatpak install -y flathub org.freedesktop.Platform//25.08
 
-python3 flatpak-pip-generator.py --requirements-file=../requirements.txt --output=pypi-dependencies.json
+# python3 flatpak-pip-generator.py \
+#     --requirements-file=../requirements.txt \
+#     --output=pypi-dependencies.json
+
+sudo flatpak-builder --force-clean build-dir ../manifest.yml
+
