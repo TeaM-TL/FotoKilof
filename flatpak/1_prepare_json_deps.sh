caa3f8f212d2c21f8dbcb927839358505cad7b9d
TLu <tlu@team-tl.pl>
2025-12-06 14:31:22 +0100

    flatpak

--- flatpak/1_prepare_json_deps.sh
+++ flatpak/1_prepare_json_deps.sh
@@ -7,6 +7,6 @@ export SSL_CERT_FILE=$(python3 -m certifi)
 curl -O https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/pip/flatpak-pip-generator.py
 chmod +x flatpak-pip-generator.py
 
-flatpak-builder --force-clean build-dir manifest.yml
+flatpak-builder --force-clean build-dir ../manifest.yml
 
 python3 flatpak-pip-generator.py --requirements-file=../requirements.txt --output=pypi-dependencies.json
